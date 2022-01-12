5796e4893eb5566f26f87c4f642fc188b977045a
Christopher Bloom <bloomcb@gmail.com>
2022-01-11 18:53:35 -0800

    prettier and some changes

--- src/project-1/app.ts
+++ src/project-1/app.ts
@@ -1,1 +1,11 @@
-console.log("hello world");
+interface BigThing {
+  blerp: string;
+  herp: string;
+}
+
+const thing: BigThing = {
+  blerp: "flerp",
+  herp: "herp",
+};
+
+console.log({ thing });
